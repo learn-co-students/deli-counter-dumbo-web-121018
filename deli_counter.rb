7f2e721d9ff758cab78602e9a97be3deadac17d5
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 19:40:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
+require 'pry'
 # Write your code here.
+def line (katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else 
+    counter = 0 
+    while counter < katz_deli.size
+      current_line = "The line is currently: "
+      current_line = current_line + counter + 1 + '. ' + katz_deli[counter]
+      counter++
+    end
+    puts current_line
+  end
+end
+
